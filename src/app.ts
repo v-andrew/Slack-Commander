8567d2083aefdde6d34beef228ba03ef7a3788a4
334504073 <andrei.varabei@rbc.com>
2019-10-21 09:41:48 -0400

    MAC OS changes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
-import { Message, Self, Team } from './lib/types';
+import { Message, Self, Team } from './lib/types'
 import { config } from 'dotenv'
 import { RTMClient, ErrorCode } from '@slack/rtm-api'
 import { WebClient, ConversationsListArguments} from '@slack/web-api'
-import { EventsHandler } from './events/handler';
-import { fromEvent, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { EventsHandler } from './events/handler'
+import { fromEvent, Observable } from 'rxjs'
+import { filter } from 'rxjs/operators'
+import HttpsProxyAgent from 'https-proxy-agent'
+config()
 
-config();
-const conversationId = 'CH0QV1CFP';
+const conversationId = 'CH0QV1CFP'
+const proxyAgent = process.env.http_proxy ? new HttpsProxyAgent(process.env.http_proxy) : null
+
 ;(async () => {
   // Start your app
-  const rtm = new RTMClient(process.env.SLACK_BOT_TOKEN)
-  const web = new WebClient(process.env.SLACK_BOT_TOKEN)
+  const rtm = new RTMClient(process.env.SLACK_BOT_TOKEN, proxyAgent ? { agent: proxyAgent }: undefined)
+  const web = new WebClient(process.env.SLACK_BOT_TOKEN, proxyAgent ? { agent: proxyAgent } : undefined)
   const handler = new EventsHandler()
   const obs = fromEvent<Message>(rtm, 'message')
   obs.subscribe(x => console.dir('message', x), e => console.error('error', e));
@@ -21,7 +24,7 @@ const conversationId = 'CH0QV1CFP';
     const userRef = `<@${(self as Self).id}>`
     const finalObs = obs.pipe(filter((msg) => msg.text.startsWith(userRef) ))
     finalObs.subscribe(x => console.log(JSON.stringify(x)), e => console.error('error', e));
-    // const responce = await rtm.sendMessage('Hello', conversationId)
+    // const response = await rtm.sendMessage('Hello', conversationId)
     handler.setup(self, team)
     console.log('ok')
   } catch (error) {
