71c0de30787c6b51148dab0521bfb79002d1246d
V. Andrew <v.andrew@live.com>
2020-04-29 14:52:40 -0400

    Override environment added

--- src/commands/etv.ts
+++ src/commands/etv.ts
@@ -3,19 +3,26 @@ import { AbstractCommand } from "./abstractCommand";
 import { MessageInfo } from "../lib/types";
 import {exec} from 'child_process';
 
-const shellCmd = `node D:\\node\\etvnet\\dist\\app.js do=etv code=`
 const command = 'etv'
 export class EtvCommand extends AbstractCommand{
+    shellCmd = `node D:\\node\\etvnet\\dist\\app.js do=etv code=`
     helpAndUsage = {usage:'`etv` `code` - registers tv device which shows `code` on the screen', command: command}
     constructor() {
         super(command)
+        if (process.env.shellCmd) this.shellCmd = process.env.shellCmd
     }
     async action(cmd: string, msgInfo:MessageInfo, params:string[]) {
         console.log(`- ${cmd} Command: [${params.join(', ')}]'`)
         await this.reply(msgInfo, `<@${msgInfo.user}>. Executing Etv`)
         try {
-            console.log( await runShellCommand(shellCmd+params[0]))
-            await this.reply(msgInfo, `Done`)
+            const fs = require('fs')
+            const dotenv = require('dotenv')
+            try {
+                const envConfig = dotenv.parse(fs.readFileSync('.env.override'))
+                if(envConfig['shellCmd']) this.shellCmd = envConfig['shellCmd']
+            } catch(ex){ console.error(ex)}
+            const shOut = await runShellCommand(this.shellCmd + params[0])
+            await this.reply(msgInfo, `Done: ${shOut}`)
         } catch (ex) {
             console.error(ex)
             await this.reply(msgInfo, `Error: ${ex}`)
