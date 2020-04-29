cfa4ae112ac0aa8d49a0946c37e0458baac635ed
V. Andrew <v.andrew@live.com>
2020-04-29 17:07:30 -0400

    =refactoring

--- src/commands/commandsRegistry.ts
+++ src/commands/commandsRegistry.ts
@@ -1,9 +1,9 @@
 import { AbstractCommand } from './abstractCommand';
 import { HelpCommand } from './help';
 import { DefaultCommand } from './default';
-import { RunCommand } from './run';
+import { RunCommand as RunLsCommand } from './runls';
 export const CommandsRegistry: AbstractCommand[] = [
     new HelpCommand,
     new DefaultCommand,
-    new RunCommand
+    new RunLsCommand
 ]
